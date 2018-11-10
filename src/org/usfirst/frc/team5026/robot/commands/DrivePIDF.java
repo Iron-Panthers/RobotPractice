be2f1d1d49c5a9e7df794108b6a0daac21d623b3
Aldric <aldricsaligo21@gmail.com>
2018-11-09 16:07:12 -0800

    Constants and DriveWithVelocity PIDF
    Added DrivePIDF command, works as DriveWithVelocity PIDF. Adds constants class that needs to be updated, just makes P, I, D, and F equal to 0.5

---
+++ src/org/usfirst/frc/team5026/robot/commands/DrivePIDF.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc.team5026.robot.commands;
+
+import org.usfirst.frc.team5026.robot.Robot;
+import org.usfirst.frc.team5026.robot.util.Constants;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DrivePIDF extends Command {
+	double desiredVelocity = 0;
+	double currentError = desiredVelocity;
+	double previousError = currentError;
+	double totalError = 0;
+	double deltaError = 0;
+	
+    public DrivePIDF(double desiredVelocity) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.drive);
+    	this.desiredVelocity = desiredVelocity;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	currentError = desiredVelocity - Robot.hardware.driveLMotor1.getSelectedSensorVelocity();
+    	deltaError = currentError - previousError;
+    	double PIDF = Constants.DRIVE_F*desiredVelocity + (currentError*Constants.DRIVE_P) + (totalError*Constants.DRIVE_I) + (deltaError*Constants.DRIVE_D);
+    	Robot.hardware.driveLMotor1.set(ControlMode.PercentOutput, PIDF);//incomplete
+    	previousError = currentError;
+    	totalError += currentError;
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ src/org/usfirst/frc/team5026/robot/util/Constants.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team5026.robot.util;
+
+public class Constants {
+	public static final double DRIVE_F = 0.5;
+	public static final double DRIVE_P = 0.5;
+	public static final double DRIVE_I = 0.5;
+	public static final double DRIVE_D = 0.5;
+	
+}
